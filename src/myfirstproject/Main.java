064a1be09416a179fd75ccd8ad582680e200e5a3
man55 <doukara001@gmail.com>
2018-09-25 08:44:22 +0100

    i complait the simple project (search in list or contents on arraylist

--- src/myfirstproject/Main.java
+++ src/myfirstproject/Main.java
@@ -12,55 +12,7 @@ public class Main {
         First obj = new First();
         obj.searchTitel();
         
-  /*      
-ArrayList <String> doc = new ArrayList <String>();
-        
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Welcome");
-       System.out.print("For Search enter [s], for add enter [a] : ");
-       String choise = sc.next();
-       if(choise.equals("s"))
-       {
-           System.out.print("Enter your words search : ");   
-           String search = sc.nextLine();
-     
-     boolean chek = false ;
-     for (int i=0 ; i<doc.size();i++)
-     {
-         if(doc.contains(search))
-         {
-             chek = true;
-            
-         }
-         if(chek == true)
-         {             
-            // get me the place number on arrayList
-          int  post = doc.indexOf(search);  
-          // for print page number
-          post = post +1;  
-        
-          System.out.println("Is her go to -> " + doc.get(post));          
-          break;      
-          
-         }
-        else
-        {
-          System.out.print("Not found try again " ); 
-         break;
-        }                 
-     }
-       }
-       else if(choise.equals("a"))
-       {
-           System.out.print("loading...! : "); 
-       }
-       else 
-           System.out.print("Your enter a wrong carchter try again "); 
-     
-     
-     
-     
-  */      
+       
         
  }
 }
